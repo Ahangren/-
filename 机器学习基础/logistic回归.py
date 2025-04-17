e9a007b34730dabd5f041f5550d0f466ebf5bc1e
Ahangren <3495901744@qq.com>
2025-04-17 21:19:16 +0800

    机器学习：logistic回归，决策树，朴素贝叶斯学习

--- 机器学习基础/logistic回归.py
+++ 机器学习基础/logistic回归.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+import random
 
 def loadDataSet():
     dataMat = []                                                        #创建数据列表
@@ -55,7 +55,47 @@ def gradAscent(dataMatIn, classLabels):
         weights = weights + alpha * dataMatrix.transpose() * error
     return weights.getA()
 
+def plotBestFit(weights):
+    dataMat, labelMat = loadDataSet()                                    #加载数据集
+    dataArr = np.array(dataMat)                                            #转换成numpy的array数组
+    n = np.shape(dataMat)[0]                                            #数据个数
+    xcord1 = []; ycord1 = []                                            #正样本
+    xcord2 = []; ycord2 = []                                            #负样本
+    for i in range(n):                                                    #根据数据集标签进行分类
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])    #1为正样本
+        else:
+            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])    #0为负样本
+    fig = plt.figure()
+    ax = fig.add_subplot(111)                                            #添加subplot
+    ax.scatter(xcord1, ycord1, s = 20, c = 'red', marker = 's',alpha=.5)#绘制正样本
+    ax.scatter(xcord2, ycord2, s = 20, c = 'green',alpha=.5)            #绘制负样本
+    x = np.arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - weights[1] * x) / weights[2]
+    ax.plot(x, y)
+    plt.title('BestFit')                                                #绘制title
+    plt.xlabel('X1'); plt.ylabel('X2')                                    #绘制label
+    plt.show()
+
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    m, n = np.shape(dataMatrix)  # 返回dataMatrix的大小。m为行数,n为列数。
+    weights = np.ones(n)  # 参数初始化
+    for j in range(numIter):
+        dataIndex = list(range(m))
+        for i in range(m):
+            alpha = 4 / (1.0 + j + i) + 0.01  # 降低alpha的大小，每次减小1/(j+i)。
+            randIndex = int(random.uniform(0, len(dataIndex)))  # 随机选取样本
+            h = sigmoid(sum(dataMatrix[dataIndex[randIndex]] * weights))  # 选择随机选取的一个样本，计算h
+            error = classLabels[dataIndex[randIndex]] - h  # 计算误差
+            weights = weights + alpha * error * dataMatrix[dataIndex[randIndex]]  # 更新回归系数
+            del (dataIndex[randIndex])  # 删除已经使用的样本
+    return weights  # 返回
+
 
 if __name__ == '__main__':
     dataMat, labelMat = loadDataSet()
-    print(gradAscent(dataMat, labelMat))
+    weights = stocGradAscent1(np.array(dataMat), labelMat)
+    plotBestFit(weights)
+
+
+
